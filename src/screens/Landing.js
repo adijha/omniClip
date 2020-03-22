39b0a7a78af4a8c18ed1db74c32e0267099ea83c
Aditya Kumar Jha <adityajha126@gmail.com>
2020-03-22 06:35:36 +0530

    structured pp

---
+++ src/screens/Landing.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const Landing = () => {
+  return (
+    <View>
+      <Text>Landing
+Landing</Text>
+    </View>
+  )
+}
+
+export default Landing
+
+const styles = StyleSheet.create({})
