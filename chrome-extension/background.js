f57c818deafd304544e0eccff933eadf6ac37152
vivek <vivekchauhaniit@gmail.com>
2020-03-27 17:01:56 +0530

    commit2

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -9,6 +9,19 @@ chrome.contextMenus.onClicked.addListener(function(clickData){
   if (clickData.menuItemId =="omniwor" && clickData.selectionText){
 
     var loge= clickData.selectionText
-    console.log(loge);
+
+    fetch('http://localhost:5000/users/add',{
+      method:'POST',
+      headers:{
+        'Content-Type':'application/json'
+      },
+      body:JSON.stringify({username:`${loge}`})
+    }).then(res=>{
+      return res.json()
+    })
+    .then(data=>console.log(data))
+    .catch(error=>console.log("error")
+    )
+    
   }
 });
