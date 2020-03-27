e35f730d87354762376ad066a56eda15c60df22b
vivek <vivekchauhaniit@gmail.com>
2020-03-27 10:32:28 +0530

    added extention

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -1,16 +1,14 @@
+var menuItem = {
+  "id":"omniwor",
+  "title":"Omnivorus",
+  "contexts":["selection"]
+};
+chrome.contextMenus.create(menuItem);
 
-'use strict';
+chrome.contextMenus.onClicked.addListener(function(clickData){
+  if (clickData.menuItemId =="omniwor" && clickData.selectionText){
 
-chrome.runtime.onInstalled.addListener(function () {
-  chrome.storage.sync.set({ color: '#3aa757' }, function () {
-    console.log('The color is green.');
-  });
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
-    chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: { hostEquals: 'developer.chrome.com' },
-      })],
-      actions: [new chrome.declarativeContent.ShowPageAction()]
-    }]);
-  });
+    var loge= clickData.selectionText
+    console.log(loge);
+  }
 });
